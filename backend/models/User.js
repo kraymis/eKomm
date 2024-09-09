84f9d6f100eedba43611e8967f710302a60ab43e
Aymen Maachi <kraymis.maachi@gmail.com>
2024-09-09 23:29:09 +0100

    modified user model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,15 +1,44 @@
+// models/User.js
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  friendRequests: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  profileImg: { type: String, default: 'D:\Fsociety\Kraymis\Front-End\JWTapp\server\pictures\profile.png' }, // Add this line for profile image
+// Define User schema
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  cart: {
+    cartItems: [
+      {
+        productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+        name: String,
+        price: Number,
+        quantity: Number
+      }
+    ],
+    total: { type: Number, default: 0 }
+  },
+  orders: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Order'  // Reference to the Order model
+    }
+  ]
+});
 
-
-}
-,{timestamps:true});
-
-module.exports = mongoose.model('User', UserSchema);
+// Create User model
+module.exports = mongoose.model('User', userSchema);
