471062ab8692c757b14fb96ae4c8a40c940d175a
Aymen Maachi <kraymis.maachi@gmail.com>
2024-09-10 23:41:36 +0100

    added some stuff

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { getProducts, getProductById } = require('../controllers/productControllers');
+const { getProducts, getProductById ,createProduct} = require('../controllers/productControllers');
 
 // Get all products
 router.get('/', getProducts);
 
 // Get a single product by ID
 router.get('/:id', getProductById);
+
+router.post('/', createProduct);
+
 
 module.exports = router;
