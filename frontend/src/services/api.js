f76983b3ec80bb9297c0065c027165d0e449ac31
Aymen Maachi <kraymis.maachi@gmail.com>
2024-09-26 08:51:17 +0100

    deplyoing webstie

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://ekomm-backend.onrender.com/api';
 const getAuthToken = () => localStorage.getItem('token'); // Retrieve token from local storage
 
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://ekomm-backend.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
